bf2938cb693c64bc7e50282fcee5b124042aa491
nitu kumari <nitu.progclasses@gmail.com>
2025-01-18 10:57:16 +0530

    first commit

---
+++ index2.js
@@ -0,0 +1,14 @@
+// NPM => node package manager :
+// it helps to install packages from node js repo.
+
+//npm install packagename
+
+//npm i packagename 
+
+//example:
+
+//npm i multer
+
+//npm unistall multer
+
+const multer = require('multer');
---
+++ sync.js
@@ -0,0 +1,25 @@
+function holdTime(){
+    let ms=4000+new Date().getTime();
+    while(new Date<ms){}
+} 
+function registration(){
+    console.log('registration sucessfully')
+}
+
+function sendEmail(){
+    console.log('send Email successfull')
+}
+
+function login(){
+    console.log('login successfully')
+}
+
+function getdata(){
+    console.log('GETDATA successful')
+}
+registration();
+sendEmail();
+login();
+getdata();
+
+console.log('other work')
